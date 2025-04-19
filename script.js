bffb2880b0640218141dc5bb39b4bd5bfe2262d7
Catalina <cattyberr@gmail.com>
2025-04-19 15:10:47 -0400

    Added Images & Renamed A Bunch Of Stuff & Redid The Nav

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const obj = { a: 13, b: 42, c: 211 };
 for (i in obj) console.log(i + ': ' + obj[i]);
 // JavaScript Document
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
